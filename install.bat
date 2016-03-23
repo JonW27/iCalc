c9b1f90aff47133b6a412aeae7e14345321d2e91
JonW27 <18wongj@gmail.com>
2016-03-22 23:32:09 -0400

    windows batch installer (includes pip)

---
+++ install.bat
@@ -0,0 +1,6 @@
+START www.lfd.uci.edu/~gohlke/pythonlibs/djcobkfp/pip-8.0.3-py2.py3-none-any.whl
+pip install virtualenv
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+icalc --help
